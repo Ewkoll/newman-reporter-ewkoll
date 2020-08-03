f15d1ca0599950e673d0b319d01ab600be35baeb
ewkoll <ideath@operatorworld.com>
2020-08-03 15:43:26 +0800

    modify

--- example/generate_htmlextra.sh
+++ example/generate_htmlextra.sh
@@ -1,1 +1,1 @@
-newman run Restful_Booker_Collection.json -e Restful_Booker_Environment.json -r newman-reporter-ewkoll
+newman run Restful_Booker_Collection.json -e Restful_Booker_Environment.json -r ewkoll
--- readme.md
+++ readme.md
@@ -7,3 +7,9 @@ npm link
 npm pack
 npm i -g *.tgz
 ```
+
+```
+npm set registry https://registry.npm.taobao.org/
+npm config list
+npm set registry https://registry.npmjs.org/
+```
